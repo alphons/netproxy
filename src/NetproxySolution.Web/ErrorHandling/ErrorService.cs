e4f0f774afd24af9be6c5d1031f0f94d24f3c2b3
alphons <alphons@heijden.com>
2024-11-22 14:17:25 +0100

    save changes not needed

--- src/NetproxySolution.Web/ErrorHandling/ErrorService.cs
+++ src/NetproxySolution.Web/ErrorHandling/ErrorService.cs
@@ -2,19 +2,8 @@
 
 namespace NetproxySolution.Web.ErrorHandling;
 
-
 public class ErrorService
 {
 	public List<WebError> DB { get; set; } = [];
-	public async Task<int> SaveChangesAsync()
-	{
-		await Task.Yield();
-
-		var id = DB.Count;
-		var list = DB.Where(x => x.Id == 0).ToList();
-		foreach (var error in list)
-			error.Id = id++;
-
-		return list.Count;
-	}
+	
 }
